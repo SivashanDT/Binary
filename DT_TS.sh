0ec2e7b184117584426c55cd66ea0abfb5d15c6c
SivashanDT <sivashan.naicker@digitaltwin.digital>
2021-06-14 03:27:50 -0700

    Updated Binary and script

--- DT_TS.sh
+++ DT_TS.sh
@@ -1,4 +1,6 @@
 #!/bin/sh /etc/rc.common
+EXTRA_COMMANDS="update"
+EXTRA_HELP=" update main code"
 
 FILE=/root/main
 if test -f "$FILE"; then
@@ -17,3 +19,8 @@ start() {
     /root/main &
 }
 
+update(){
+    echo update
+    cd /root/ && curl -OL https://github.com/SivashanDT/Binary/raw/main/main
+    chmod +x /root/main
+}
